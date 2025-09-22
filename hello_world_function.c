8258910f41fde639fc5028dc223521b3f1ca2636
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-09-21 21:21:38 -0700

    Add files via upload

---
+++ hello_world_function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Define a function named 'greet' that prints greetings
+void greet() { 
+    printf("Hello, World!\n");
+    printf("Hello, COMP106!\n");
+}
+
+int main() { // The entry point of the program
+    greet(); // Call the greet() function to run its code
+    return 0;
+}

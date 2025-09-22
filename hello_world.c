3fa11372402f925a505271c5d2af0317a64af001
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-09-21 21:03:28 -0700

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,6 @@
-#include <stdio.h> // Include the standard input/output library
+#include <stdio.h> // Include the standard input/output library to use printf()
 
-int main() {
+int main() { // Start of the main function, the entry point of the program
     printf("Hello, World!\n"); // Print out "Hello, World!"
-    return 0; // Indicate that the program finished successfully
-}
+    return 0; // Return 0 to the operating system to indicate that the program finished successfully
+} // End of the main function

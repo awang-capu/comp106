68859254d0d32c40bc603dffffbee2c178c96387
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-09-21 19:03:43 -0700

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Include the standard input/output library
+
+int main() {
+    printf("Hello, World!\n"); // Print out "Hello, World!"
+    return 0; // Indicate that the program finished successfully
+}

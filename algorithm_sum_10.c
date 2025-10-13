c960410f0d38479578da02c9aaa5c5e3bf3697bc
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-10-13 14:40:13 -0700

    Update algorithm_sum_10.c

--- algorithm_sum_10.c
+++ algorithm_sum_10.c
@@ -5,7 +5,7 @@ int main() {
     int sum = 0;
     for (int i = 1; i <= 10; i++) 
         sum += i;
-    printf("The sum from 1 to 10 is: %d.\n", sum);
+    printf("The sum of integers from 1 to 10 is: %d.\n", sum);
 
     return 0;
 }

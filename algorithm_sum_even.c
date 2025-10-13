c7de73b169929abb66976c81ebdd43bf5fbf3eb4
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-10-13 14:40:36 -0700

    Update algorithm_sum_even.c

--- algorithm_sum_even.c
+++ algorithm_sum_even.c
@@ -5,7 +5,7 @@ int main() {
     int sum = 0;
     for (int i = 2; i <= 10; i = i + 2) 
         sum += i;
-    printf("The sum of even numbers from 1 to 10 is: %d.\n", sum);
+    printf("The sum of even integers from 1 to 10 is: %d.\n", sum);
 
     return 0;
 }

8c1ee873780cb15b4d169480db4c84b32c36d39b
awang-capu <149170292+awang-capu@users.noreply.github.com>
2025-10-22 12:03:30 -0700

    Create lab4_solution.c

---
+++ Sparki/lab4_solution.c
@@ -0,0 +1,33 @@
+#include <Sparki.h> // Include the sparki library
+
+const int BACKWARD_DISTANCE = 5;
+const int ANGLE = 20;
+const int THRESHOLD = 200; // If below this value, no surface underneath
+
+void setup() {}
+
+void loop() {
+    sparki.RGB(RGB_GREEN); // Turn the light green
+    int edge_left   = sparki.edgeLeft();   // Measure the left edge IR sensor
+    int edge_right  = sparki.edgeRight();  // Measure the right edge IR sensor
+    
+    if (edge_left < THRESHOLD) { // If no surface underneath left sensor
+        sparki.moveBackward(BACKWARD_DISTANCE);
+        sparki.moveRight(ANGLE); 
+    } else if (edge_right < THRESHOLD) { // If no surface underneath right sensor
+        sparki.moveBackward(BACKWARD_DISTANCE);
+        sparki.moveLeft(ANGLE);
+    }
+
+    int dist_cm = sparki.ping(); // Measures the distance with Sparki's eyes
+    if(dist_cm != -1 && dist_cm < 20) {
+        sparki.RGB(RGB_RED); 
+        sparki.beep(); 
+        sparki.moveBackward(10);
+        sparki.moveRight(30);
+        delay(1000);
+    }
+
+    sparki.moveForward();
+    delay(100);
+}
